e22114eee029893ee2875267f858652b1679fad2
zasyxmi <networkasyraf@gmail.com>
2026-05-14 18:19:50 +0800

    Update logo and splashscreen image

---
+++ app/src/main/java/com/example/smartpetrolcostcalculator/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.smartpetrolcostcalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DELAY);
+    }
+}
